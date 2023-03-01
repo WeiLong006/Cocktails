e5bc986cddd2519be59410ab9de2ea58b4830702
WeiLong006 <lim.wei.long.1@gmail.com>
2023-03-01 15:21:12 +0800

    search still not working

--- Cocktail/src/app/admin-page/admin-page.component.ts
+++ Cocktail/src/app/admin-page/admin-page.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.component.html',
-  styleUrls: ['./admin-page.component.css']
+  styleUrls: ['./admin-page.component.css'],
 })
 export class AdminPageComponent {
+  searchInput = '';
 
+  getInput(e: any) {
+    this.searchInput = e.target.value;
+  }
+
+  getSearch() {
+    console.log(this.searchInput);
+  }
 }
