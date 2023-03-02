55fd5a75c56418ebeb9a13ae0543e711feabbf6c
WeiLong006 <lim.wei.long.1@gmail.com>
2023-03-02 13:07:08 +0800

    mapping for search complete

--- Cocktail/src/app/admin-page/admin-page.component.ts
+++ Cocktail/src/app/admin-page/admin-page.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+export interface Response {
+  name: any;
+}
 
 @Component({
   selector: 'app-admin-page',
@@ -7,12 +12,30 @@ import { Component } from '@angular/core';
 })
 export class AdminPageComponent {
   searchInput = '';
+  // searchResult: any[] = [];
+  searchResult: any;
 
   getInput(e: any) {
     this.searchInput = e.target.value;
   }
 
   getSearch() {
-    console.log(this.searchInput);
+    // console.log(this.searchInput);
+    this.searchCocktail();
+  }
+
+  constructor(private http: HttpClient) {}
+
+  searchCocktail() {
+    let search = new HttpHeaders({ name: this.searchInput });
+    this.http
+      .post<Response>('http://127.0.0.1:5001/fave/search', {
+        name: this.searchInput,
+      })
+      .subscribe((data) => {
+        this.searchResult = data;
+        console.log(this.searchResult);
+        console.log(this.searchResult[0].image);
+      });
   }
 }
